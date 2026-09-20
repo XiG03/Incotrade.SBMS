1a6ef85e2b31377065a00ad982e70eabb6c947ce
CIY <qnhat0312@gmail.com>
2026-09-20 09:51:59 +0700

    (feat/Services)#001: Develop Services function (tested)

---
+++ sbms.backend/Modules/Service/DTOs/ServiceUpdateRequest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace sbms.backend.Modules.Services.DTOs
+{
+    public class ServicesUpdateRequest
+    {
+        [Required]
+        public Guid Id {get; set;}
+        [Required]
+        public string Name {get; set;}
+        public string Description {get; set;}
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Duration must be greater than 0.")] // Thoi gian phai lon hon 0
+        public decimal DurationMinutes {get; set;}
+        [Required]
+        [Range(0, double.MaxValue, ErrorMessage ="Price must be greater than 0.")] // Gia khong duoc am
+        public decimal Price {get; set;}
+        public bool IsActive {get; set;}
+    }
+}
+
+
---
+++ sbms.backend/Modules/Service/Services/IServicesService.cs
@@ -0,0 +1,16 @@
+﻿using sbms.backend.Helpers;
+using sbms.backend.Modules.Services.DTOs;
+
+namespace sbms.backend.Modules.Services.Services
+{
+    public interface IServiceService
+    {
+        public Task<APIResponse<ServicesCreateResponse>> CreateServiceAsync(ServicesCreateRequest request);
+        public Task<APIResponse<ServicesUpdateResponse>> UpdateServiceAsync(ServicesUpdateRequest request);
+        public Task<APIResponse<List<ServicesResponse>>> GetAllServicesAsync();
+        public Task<APIResponse<List<ServicesResponse>>> GetActiveServicesAsync();
+        public Task<APIResponse<List<ServicesResponse>>> GetInActiveServicesAsync();
+
+
+    }
+}
